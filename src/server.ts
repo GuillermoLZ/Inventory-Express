1c317e9a0525833e2f507726c585a5864440aaea
GuillermoLZ <130412287+GuillermoLZ@users.noreply.github.com>
2026-04-25 17:11:02 -0500

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,37 @@ import authRoutes from "./modules/auth/presentation/routes/auth.routes"
 import userRoutes from "./modules/users/presentation/routes/user.routes"
 import { errorMiddleware } from "@shared/middlewares/error.middleware"
 import { httpLogger } from "@shared/middlewares/http-logger.middleware"
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+app.use((req, res, next) => {
+  console.log('Origin:', req.headers.origin);
+  next();
+});
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}))
+app.options('*', cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+// app.use(cors())
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(204) // 🔥 CLAVE
+//   }
+
+//   next()
+// })
 
 app.use(httpLogger)
 app.use(express.json())
