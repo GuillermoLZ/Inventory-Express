77c184647fb68389dc72fd4d34888fa0155ccc6e
GuillermoLZ <130412287+GuillermoLZ@users.noreply.github.com>
2026-04-26 03:26:25 -0500

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import "dotenv/config"
 import express from "express"
 import productRoutes from "./routes/product.routes"
 import { initDB } from './lib/db'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 3000
-
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
 app.use(express.json())
 app.use("/api/products", productRoutes)
 
