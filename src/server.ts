19dff511d6b31c72ac21dba0d4d6fcf8fa0ed44c
GuillermoLZ <130412287+GuillermoLZ@users.noreply.github.com>
2026-04-25 23:26:37 -0500

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -11,33 +11,16 @@ import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 3000
-app.use((req, res, next) => {
-  console.log('Origin:', req.headers.origin);
-  next();
-});
-app.use(cors({
-  origin: ['http://localhost:5173'],
+
+const corsConfig = {
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}))
-app.options('*', cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+  credentials: true // si usas cookies/tokens
+};
 
-// app.use(cors())
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
-//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-
-//   if (req.method === 'OPTIONS') {
-//     return res.sendStatus(204) // 🔥 CLAVE
-//   }
-
-//   next()
-// })
+app.use(cors(corsConfig));
+app.options('*', cors(corsConfig)); // preflight global
 
 app.use(httpLogger)
 app.use(express.json())
@@ -54,6 +37,6 @@ const server = http.createServer(app)
 initSocket(server)
 
 // Listen Server
-server.listen(PORT, () => {
+server.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })
